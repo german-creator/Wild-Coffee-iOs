aea0441106a094ca12c11cb9667341f4e1acacca
german-creator <zennex.test1@gmail.com>
2020-05-01 13:48:07 +0700

    Initial Commit

---
+++ Wild App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wild App
+//
+//  Created by Герман Иванилов on 01.05.2020.
+//  Copyright © 2020 Герман Иванилов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
